87fc3ace87319afa13d3041345374ecfff8816c4
Tomáš Martinovič <tomas.martinovic@vsb.cz>
2021-04-07 10:26:59 +0200

    Update package name in app_sys

--- dev/01_start.R
+++ dev/01_start.R
@@ -14,7 +14,7 @@
 ## Fill the DESCRIPTION ----
 ## Add meta data about your application
 golem::fill_desc(
-  pkg_name = "msk.covid.app", # The Name of the package containing the App 
+  pkg_name = "NTS.shiny", # The Name of the package containing the App 
   pkg_title = "Vaccination center simulator", # The Title of the package containing the App 
   pkg_description = "This package contains sihny app for simulation of the COVID vaccination center in Moravian region.", # The Description of the package containing the App 
   author_first_name = "Tomas", # Your First Name
@@ -28,7 +28,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( name = "Tomas Martinovic" )  # You can set another license here
+# usethis::use_mit_license( name = "Tomas Martinovic" )  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
